bec58469024c1cdf3baa8c29346bc9884b4af989
youssefshaban <youssef.shaban.atta@gmail.com>
2021-04-15 14:02:35 +0200

    init2

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -12,11 +12,8 @@ export class UsersComponent {
   message: string;
 
   users: User[] = [
-    { name: 'muhammad', age: 25, job: 'student' },
-    { name: 'abdelhay', age: 55, job: 'doctor' },
     { name: 'youssef', age: 26, job: 'engineer' },
-    { name: 'ali', age: 24 },
-    { name: 'samir', age: 22, job: 'developer' },
+    { name: 'muhammad', age: 25, job: 'student' },
   ];
 
 
